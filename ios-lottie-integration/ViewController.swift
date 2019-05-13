186ce2c525501a0f1d58cc6a4b91ffce180fbd38
Juan Cruz Guidi <jguidi@rccl.com>
2019-05-13 10:54:19 -0300

    Initial Commit

---
+++ ios-lottie-integration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-lottie-integration
+//
+//  Created by Tomas Mateo Perotti on 13/05/2019.
+//  Copyright © 2019 Tomas Mateo Perotti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
